6789e6ab3b1b0ce710c50cbf07d7193b92422f82
Ushio <109308101+Stoforv2@users.noreply.github.com>
2024-01-19 19:56:08 +0800

    Update realm.sh

--- realm.sh
+++ realm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
